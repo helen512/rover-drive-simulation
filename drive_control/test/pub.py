82bd3089abe2d94d4ce6de924fe0a5f3c0ecfa03
Mingyue Deng <helendeng512@gmail.com>
2022-11-05 16:42:03 -0400

    Fixed rover back wheels  problem + tried to fix angular problem

--- drive_control/test/pub.py
+++ drive_control/test/pub.py
@@ -6,23 +6,26 @@ from drive_control.msg import WheelSpeed
 
 def pub():
     robot_twist = Twist()
-    robot_twist.linear.x = 10
-    robot_twist.angular.z = 20
+    robot_twist.angular.z = 45
+    robot_twist.linear.x = 30
+    # robot_twist.angular.z = 1500
     twist_pub = rospy.Publisher("rover_velocity_controller/cmd_vel", Twist, queue_size=10)
     
     feedback = WheelSpeed()
-    feedback.left[0] = 20
-    feedback.left[1] = 22
-    feedback.right[0] = 18
-    feedback.right[1] = 20
+    feedback.left[0] = 0
+    feedback.left[1] = 0
+    feedback.right[0] = 200
+    feedback.right[1] = 200
     feedback_pub = rospy.Publisher('/feedback_velocity', WheelSpeed, queue_size=10)
 
     rospy.init_node('test_pub', anonymous=True)
     i = 0
     while not rospy.is_shutdown():
+        robot_twist.linear.x = 30 + i
         twist_pub.publish(robot_twist)
         feedback_pub.publish(feedback)
-        time.sleep(1)
+        time.sleep(0.5)
+        i += 10
         
 
 
